257c1eb0810b8d767cba2580f16795b795625d9f
Athena Krikorian <athenakrik@new-mac-2055.local>
2026-06-26 17:07:48 -0400

    style: tighten vertical spacing between arrow icons and i stems

--- components/IntrisWordmark.tsx
+++ components/IntrisWordmark.tsx
@@ -11,7 +11,7 @@ const ArrowI = () => (
       xmlns="http://www.w3.org/2000/svg"
       style={{
         position: 'absolute',
-        top: '0.07em',
+        top: '0.13em',
         left: '50%',
         transform: 'translateX(-50%)',
         width: '0.15em',
